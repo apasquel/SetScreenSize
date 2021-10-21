b476ae2ba79dd0e05d699e0ed1b1671b0f71800c
Cookiez152 <49290434+Cookiez152@users.noreply.github.com>
2021-10-21 13:45:36 -0400

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Script to Resize VM Screen. Place script to autostart on startup
-#Austin Pasquel
 #Source: Some Ubuntu Forum I couldn't find again. 
 gtf 1920 1080 60
 xrandr --newmode "1920x1080_60.00"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync
